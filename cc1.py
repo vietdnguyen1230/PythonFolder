17fb0c37a74d858eab6d46193bb15062c5279747
Viet Nguyen <vietnguyen2330@gmail.com>
2022-12-22 05:50:20 -0800

    first commit

---
+++ cc1.py
@@ -0,0 +1,15 @@
+# Turn this line into a comment
+
+cities = ["Seattle", "Tacoma", "Bellevue"]
+
+a = 1
+print(type(a))
+
+b = 3.99
+print(type(b))
+
+c = "word"
+print(type(c))
+
+d = a + b
+print(d)
