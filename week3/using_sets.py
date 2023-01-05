05cdbecff146fa12798f077c44cbe13184dee964
Viet Nguyen <vietnguyen2330@gmail.com>
2023-01-04 19:13:04 -0800

    1/4/23

---
+++ week3/using_sets.py
@@ -0,0 +1,9 @@
+# numbers_set = {1, 2, 3, 4, 4}
+# numbers_set = {1,2,3,4,(5,6)}
+# print(numbers_set)
+
+words_set = {"Alpha", "Bravo", "Charlie"}
+abcd = ""
+for word in words_set:
+    abcd += word
+print(abcd)
---
+++ week3/using_tuples.py
@@ -0,0 +1,7 @@
+my_tuple = (1, 2, 3, 4, 5)
+
+print(my_tuple[-1] + my_tuple[-2])
+
+my_tuple = (5, 4, 3, 2, 1)
+
+print(my_tuple)
