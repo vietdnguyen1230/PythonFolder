b1f9077af4dccc66cc7e8e71a7118ede7ca39706
Viet Nguyen <vietnguyen2330@gmail.com>
2023-01-07 14:51:11 -0800

    1/7/23

---
+++ week3/workshop3/app.py
@@ -0,0 +1,37 @@
+from donations_pkg.homepage import show_homepage, donate, show_donations
+from donations_pkg.user import login, register
+
+database = {"admin": "password123"}
+donations = []
+authorized_user = ""
+
+while True:
+    if authorized_user == "":
+        print("You must be logged in to donate.")
+    else:
+        print("Logged in as:", authorized_user)
+    show_homepage()
+    option = input("Choose an option: ")
+    if option == "1":
+        username = input("Enter username: ")
+        password = input("Enter password: ")
+        authorized_user = login(database, username, password)
+        print("login auth_user", authorized_user)
+    elif option == "2":
+        username = input("Enter username: ")
+        password = input("Enter password: ")
+        authorized_user = register(database, username)
+        print("register auth_user", authorized_user)
+        if authorized_user != "":
+            database[username] = password
+    elif option == "3":
+        if authorized_user == "":
+            print("You are not logged in.")
+        else:
+            donation = donate(username)
+            donations.append(donation)
+    elif option == "4":
+        show_donations(donations)
+    elif option == "5":
+        print("Leaving DonateMe....")
+        quit()
